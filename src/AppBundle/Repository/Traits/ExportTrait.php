70a1b6f6a7bfe0e3087f91575934a9f56d6703e6
Nicholas Gritsyuk <gritsuk57@gmail.com>
2018-01-31 14:40:58 +0200

    Fix trait

--- src/AppBundle/Repository/Traits/ExportTrait.php
+++ src/AppBundle/Repository/Traits/ExportTrait.php
@@ -3,12 +3,13 @@
 namespace AppBundle\Repository\Traits;
 
 use AppBundle\Entity\User;
-use Doctrine\ORM\QueryBuilder;
 
 trait ExportTrait
 {
-    public function getData(User $user, QueryBuilder $qb)
+    public function getCostsForLastMonth(User $user)
     {
+        $qb = $this->createQueryBuilder("entity");
+
         $result = $qb->select(array("entity"))
             ->where($qb->expr()->eq('entity.user', $user->getId()))
             ->getQuery()
